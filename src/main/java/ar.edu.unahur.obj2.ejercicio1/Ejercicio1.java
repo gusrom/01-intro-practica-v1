7d88c7171edacb7dd4025c5758ebe061f4a04f2e
Roman <r_af_95@hotmail.com>
2020-04-15 21:37:13 -0300

    Ejercicio 1

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -1,8 +1,32 @@
 package ar.edu.unahur.obj2.ejercicio1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Ejercicio1 {
 
-    public int[] ordenar() {
-        return new int[] {};
+    public static void main(String[] args) {
+        int[] l1= ingresarNumeros();
+    }
+
+    public static int[] ingresarNumeros(){
+        Scanner entrada = new Scanner(System.in);
+        int longitud =entrada.nextInt();
+        int [] vector = new int [longitud];
+        for(int i=0; i<vector.length;i++){
+            Scanner numeros= new Scanner(System.in);
+            vector[i]=entrada.nextInt();
+        }
+        return vector;
+    }
+    public int[] ordenar(int [] lista) {
+        int j=0;
+        int [] vectorInvertido=new int[lista.length];
+        for(int i=lista.length-1;i==0;i--){
+            vectorInvertido[j]=lista[i];
+        }
+        return vectorInvertido;
     }
 }
